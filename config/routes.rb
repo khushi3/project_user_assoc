d86511f360e2daa33c3ff08c57db4914e016d95a
Khushboo Bharti <khushboo.barathi@emids.com>
2015-05-26 13:15:26 -0800

    authentication using devise rails engine added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :users
   resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
